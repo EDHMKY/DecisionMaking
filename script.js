c8465d558f3cc4dfd1f4a614f7732d09b0a8478e
EDHMKY <edhm01@gmail.com>
2021-09-30 11:07:33 -0400

    Node is so confusing.

---
+++ script.js
@@ -0,0 +1,80 @@
+
+
+
+let randomNum = Math.floor(Math.random() * 5 + 1);
+
+console.log(randomNum);
+
+
+if (randomNum >= 4) {
+  console.log("Greater than or equal to 4");
+} else if (randomNum === 2 || randomNum === 3) {
+  console.log("Equal to 2 or 3");
+} else {
+  console.log("Equal to 1");
+}
+
+
+if (randomNum >= 4) {
+
+  console.log("Greater than or equal to 4");
+} else if (randomNum >= 2) {
+
+  console.log("Equal to 2 or 3");
+} else {
+  console.log("Equal to 1");
+}
+
+
+if (randomNum !== 3){
+  console.log("Not equal to 3");
+}
+
+
+
+if (randomNum === 1 || randomNum === 2 || randomNum === 4 || randomNum === 5) {
+  console.log("Not equal to 3");
+}
+
+
+if (randomNum !== 3 && randomNum !== 5){
+  console.log("NOT equal to 3 AND not equal to 5");
+}
+
+
+
+if (randomNum === 1 || randomNum === 2 || randomNum === 4) {
+console.log(randomNum);
+  console.log("NOT equal to 3 AND not equal to 5");
+}
+
+
+if (randomNum === 2 || randomNum === 4) {
+  console.log("Equal to 2 or equal to 4");
+}
+
+
+randomNum >= 4 ? console.log("Greater than or equal to 4") : console.log("Less than 4");
+
+
+console.log(randomNum >= 4 ? "Greater than or equal to 4" : "Less than 4");
+
+
+switch (randomNum){
+  case 1:
+    console.log("One");
+    
+    break;
+  case 2:
+    console.log("Two");
+    break;
+  case 3:
+    console.log("Three");
+    break;
+  case 4:
+    console.log("Four");
+    break;
+  case 5:
+    console.log("Five");
+  
+}
